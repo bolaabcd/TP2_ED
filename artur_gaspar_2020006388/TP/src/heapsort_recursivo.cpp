057f0b1a902af64012aa4564a500e5c83a5f0c8e
Artur <arthur.gaspar2011@gmail.com>
2022-01-05 20:41:16 -0300

    Arrumado heapsort recursivo.

--- artur_gaspar_2020006388/TP/src/heapsort_recursivo.cpp
+++ artur_gaspar_2020006388/TP/src/heapsort_recursivo.cpp
@@ -11,7 +11,7 @@ Heapsort_Recursivo::Heapsort_Recursivo(){}
 
 void Heapsort_Recursivo::ordena(){    
     for (int esq = (this->numero_urls)/2+1; esq >= 1; esq--) {
-        refaz(esq,this->numero_urls);
+        this->refaz(esq,this->numero_urls);
     }
 
     for (int dir = (this->numero_urls); dir > 1; ) {
@@ -20,34 +20,33 @@ void Heapsort_Recursivo::ordena(){
         this->lista_urls[1-1] = this->lista_urls[dir-1];
         this->lista_urls[-1+dir--] = primeiro; 
 
-        refaz(1, dir);
+        this->refaz(1, dir);
     }
 }
 
 void Heapsort_Recursivo::refaz(int l, int tam){
     int pai = l;
     URL_Acessos filhoR, filhoL;
+    int menor = 2*l;
+
     if (2*l+1 <= tam) {
         filhoL = this->lista_urls[2*l+1-1];
         filhoR = this->lista_urls[2*l-1];
+        if (filhoL < filhoR) {
+            menor = 2*l+1;
+        }
     } else if (2*l <= tam) {
         filhoR = this->lista_urls[2*l-1];
     } else {
         return;
     }
 
-    int maior = 2*l+1;
-
-    if (filhoL < filhoR) {
-        maior = 2*l;
-    }
-
-    if (this->lista_urls[pai-1] < this->lista_urls[maior-1]) {
+    if (this->lista_urls[menor-1] < this->lista_urls[pai-1]) {
         // Trocando elementos
         URL_Acessos url_pai = this->lista_urls[pai-1];
-        this->lista_urls[pai-1] = this->lista_urls[maior-1];
-        this->lista_urls[maior-1] = url_pai;
+        this->lista_urls[pai-1] = this->lista_urls[menor-1];
+        this->lista_urls[menor-1] = url_pai;
 
-        refaz(maior,tam);
+        refaz(menor,tam);
     }
 }
